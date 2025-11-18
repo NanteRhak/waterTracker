b93dbe3b9c1061447ec3c573d8f43fcaca836f64
Nante Rhak <nantherak@gmail.com>
2025-11-18 17:06:42 +0300

    Ajout de nouvelle fonctionnalités: 	Ajouteur l'eau pris, 	Objectif 	Voir le total d'eau pris pendant une journée

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from config import *
+from flask_wtf import FlaskForm
+from wtforns import DecimalField, SelectField, SubmitField
+from wtform.validators import DataRequired, NumberRange
 
 app = Flask(__name__)
-app.secret_key = "very-hard-to-guess"
+app.config['SECRET_KEY'] = 'secret_key']
 bootstrap = Bootstrap5(app)
 
-@app.route('/')
+class WaterIntake(FlaskForm):
+    quantity = DecimalField('Quantité', Validators=[DataRequired(), NumberRange(min=0.1, max=10)])
+    unit = SelectField('Type',choice=['mL','verre'])
+    submit = SubmitField('Ajouter')
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = WaterIntake()
+    if form.validate_on_submit():
+        
+    return render_template('index.html',form=forn,quantity=quantity, unit=unit, submit=submit)
 
 @app.route('/history')
 def history():
