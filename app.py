f91348133c1b8e9309953bb89960a993a886a007
Nante Rhak <nantherak@gmail.com>
2025-11-19 05:27:15 +0300

    Petite vérification terminé

--- app.py
+++ app.py
@@ -1,25 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, flash
 from flask_bootstrap import Bootstrap5
 from config import *
 from flask_wtf import FlaskForm
-from wtforns import DecimalField, SelectField, SubmitField
-from wtform.validators import DataRequired, NumberRange
+from wtforms import DecimalField, SelectField, SubmitField
+from wtforms.validators import DataRequired, NumberRange
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key']
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 bootstrap = Bootstrap5(app)
 
 class WaterIntake(FlaskForm):
-    quantity = DecimalField('Quantité', Validators=[DataRequired(), NumberRange(min=0.1, max=10)])
-    unit = SelectField('Type',choice=['mL','verre'])
+    quantity = DecimalField('Quantité', validators=[DataRequired(), NumberRange(min=0.1)])
+    unit = SelectField('Type',choices=[('mL','mL'),('verre','verre')])
     submit = SubmitField('Ajouter')
+
+def init_session():
+    if 'current_total' not in session:
+        session['current_total'] = 0
+    if 'history' not in session:
+        session['history'] = []
+
+def convert_to_ml(quantity, unit):
+    if unit == 'verre':
+        return quantity * 250
+    return quantity
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    init_session()
+
     form = WaterIntake()
     if form.validate_on_submit():
-        
-    return render_template('index.html',form=forn,quantity=quantity, unit=unit, submit=submit)
+        quantity = float(form.quantity.data)
+        unit = form.unit.data
+        quantity_ml = convert_to_ml(quantity, unit)
+
+        entry = {
+                    'quantity': quantity,
+                    'unit': unit,
+                    'quantity_ml': quantity_ml,
+                    'total_after': session['current_total']
+                }
+
+        session['current_total'] += quantity_ml
+        session['history'].append(entry)
+        return redirect(url_for('index'))
+        session.modified = True
+        flash(f'{quantity} {unit} ajouté\n Total: {session["current_total"]} mL', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('index.html',form=form,quantity=session.get('quantity'), unit=session.get('unit'))
 
 @app.route('/history')
 def history():
